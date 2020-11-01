bb84bceb53f350c41fc71f8ed89d7a988c072302
student <rubenabustamante@gmail.com>
2020-10-31 18:56:01 -0600

    added state machines in for switches in wd interrupt handler

--- project/src/wdInterruptHandler.c
+++ project/src/wdInterruptHandler.c
@@ -8,24 +8,35 @@
 
 #include "buzzer.h"
 
+#include "switches.h"
+
+
 void
 
 __interrupt_vec(WDT_VECTOR) WDT(){/* 250 interrupts/sec */
-
   static char blink_count = 0;
+  
 
-  if((++blink_count %25)==0){
-
-    buzzer_advance();
-
-  }
-
-  if(blink_count == 250){
-
-    main_state_advance();
-
-    blink_count =0;
-
-  }
-
+  
+    if ((switch_state_changed ==0) &&(++blink_count !=125)){
+      
+      switch_interrupt_handler();
+      blink_count = 0;
+    }
+    else{
+      toggle_red_green();
+	
+     }
+    
+    if((switch_state_changed == 1) &&(++blink_count %25)==0){
+      switch_interrupt_handler();
+      blink_count =0;
+    }
+    else{
+      toggle_red_green();
+    }
+   
 }
+    
+
+    
