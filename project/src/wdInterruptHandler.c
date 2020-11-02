a0761b54fbb9393ca322e1c007b91891eee6399b
student <rubenabustamante@gmail.com>
2020-11-01 19:25:04 -0700

    add state machince to keep track of switch and perfomr its duty in wdih

--- project/src/wdInterruptHandler.c
+++ project/src/wdInterruptHandler.c
@@ -16,27 +16,37 @@ void
 __interrupt_vec(WDT_VECTOR) WDT(){/* 250 interrupts/sec */
   static char blink_count = 0;
   
-
-  
-    if ((switch_state_changed ==0) &&(++blink_count !=125)){
+  if(++blink_count ==125){
+    toggle_red_green();
+    switch(switch_state_changed){
+      
+    /*  sw1 is pressed*/
+    case 0:
+    
+      switch_interrupt_handler();
+      switch_state_changed =0;
+      break;
+    /*sw2 is pressed*/
+    case 1:
+    
+       switch_interrupt_handler();
+       switch_state_changed =1;
+       break;
+    /*sw3 is prested*/
+    case 2:
       
       switch_interrupt_handler();
-      blink_count = 0;
-    }
-    else{
-      toggle_red_green();
-	
-     }
-    
-    if((switch_state_changed == 1) &&(++blink_count %25)==0){
+      switch_state_changed =2;
+      break;
+    case 3:
+      
       switch_interrupt_handler();
-      blink_count =0;
+      switch_state_changed =0;
+      break;   
     }
-    else{
-      toggle_red_green();
-    }
-   
+  }
 }
+  
     
 
     
