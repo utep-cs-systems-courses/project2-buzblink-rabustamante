05220e8ad13e7f42568036e37fdab4da8220aa58
student <rubenabustamante@gmail.com>
2020-11-01 19:23:11 -0700

    added 4 var to keep track of switches in switches.h

---
+++ project/src/switches.h
@@ -0,0 +1,18 @@
+#ifndef switches_included
+#define switches_included
+
+#define SW1 BIT0 /* S1 is p2.0 */
+#define SW2 BIT1 /* S2 is p2.1 */
+#define SW3 BIT2 /* s3 is p2.2 */
+#define SW4 BIT3 /* S4 is p2.3 */
+#define SWITCHES (SW1 | SW2 | SW3 | SW3) /* only 4 switch on green board */
+
+
+
+void switch_init();
+void switch_interrupt_handler();
+
+
+extern char switch_state_down1,switch_state_down2,switch_state_down3,switch_state_down4, switch_state_changed; /* effectively boolean */
+
+#endif // included
